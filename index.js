27e268dcdf39f934818c7c91ebcef0c1f7c66203
Nicholas Altunian <naaltunian@icloud.com>
2022-02-13 07:26:53 -0500

    set up server

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+require('dotenv').config()
 const express = require('express')
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page Not Found</h1>')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
